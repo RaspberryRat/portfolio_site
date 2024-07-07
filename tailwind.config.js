f70c00bb5caaffc6c62599dd32296c02079ff751
squid <raspberryratman@gmail.com>
2024-07-07 17:06:18 -0600

    animated powerbutton and sunken image effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,27 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      keyframes: {
+        scale50: {
+          '0%': {transform: 'scale(1)'},
+          '50%': {transform: 'scale(1.5)'},
+          '100%': {transform: 'scale(1)'},
+        },
+        scale0: {
+          '0%': {transform: 'scale(1)'},
+          '100%': {transform: 'scale(0)'},
+        }
+      },
+      animation: {
+        scale: 'scale50 3s ease-in-out infinite',
+        poweroff: 'scale0 50ms ease-out'
+      },
       boxShadow: {
         'inner-xl': 'inset 0px 0px 50px 10px rgba(0, 0, 0, 0.3)',
+        'sunken': 'inset 0px 10px 3px -5px #000,' +
+            'inset -10px 0px 3px -5px #000,' +
+            'inset 0px -10px 3px -5px #000,' +
+            'inset 10px 0px 3px -5px #000',
         'inner-lg': 'inset 0px 0px 20px 10px rgba(0, 0, 0, 0.3)',
       },
       colors: {
