ea9fe55f0f7bdd64fa92679517efed5e5c9aaa20
squid <raspberryratman@gmail.com>
2024-07-07 15:43:16 -0600

    setup basic computer look

---
+++ README.md
@@ -0,0 +1,1 @@
+<a target="_blank" href="https://icons8.com/icon/uI4k7cPiwxCJ/power-off-button">Power Off Button</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      boxShadow: {
+        'inner-xl': 'inset 0px 0px 50px 10px rgba(0, 0, 0, 0.3)',
+        'inner-lg': 'inset 0px 0px 20px 10px rgba(0, 0, 0, 0.3)',
+      },
       colors: {
         'main-yellow': '#fecd45',
         'alt-blue': '#2568fb',
+        'pale-brown': '#C1A17B'
       },
       backgroundImage: {
-        'custom-gradient': 'linear-gradient(to bottom right, #fecd45 49%, white 40%)'
+        'custom-gradient': 'linear-gradient(to bottom right, #fecd45 49%, white 40%)',
+        'matt-gradient': 'linear-gradient(145deg, #d6bfae, #c3a594)',
       },
     },
   },
