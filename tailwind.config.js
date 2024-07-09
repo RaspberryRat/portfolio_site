edfc7091295ca1107b2b46cbadb78376d5d65593
Raspberry Rat <raspberryratman@gmail.com>
2024-07-09 15:07:52 -0600

    added folders for projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,33 @@
 module.exports = {
   purge: ['./*.html'],
   safelist: [
-      'animate-turnOff',
-      'animate-turnOn'
+    'animate-turnOff',
+    'animate-turnOn'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontSize: {
+        'xxs': '0.5rem'
+      },
       keyframes: {
         scale50: {
-          '0%': {transform: 'scale(1)'},
-          '50%': {transform: 'scale(1.5)'},
-          '100%': {transform: 'scale(1)'},
+          '0%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.5)' },
+          '100%': { transform: 'scale(1)' },
         },
         scale0: {
-          '0%': {transform: 'scale(1)'},
-          '100%': {transform: 'scale(0)'},
+          '0%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(0)' },
 
         },
         tvOff: {
-          '0%': {transform: 'scale(1, 1)', filter: 'brightness(100%)' },
+          '0%': { transform: 'scale(1, 1)', filter: 'brightness(100%)' },
           '100%': { transform: 'scale(0.6, 0)', filter: 'brightness(800%)' }
         },
         tvOn: {
           '0%': { transform: 'scale(0.6, 0)', filter: 'brightness(800%)' },
-          '100%': {transform: 'scale(1, 1)', filter: 'brightness(100%)' }
+          '100%': { transform: 'scale(1, 1)', filter: 'brightness(100%)' }
         }
       },
       animation: {
@@ -37,9 +40,9 @@ module.exports = {
       boxShadow: {
         'inner-xl': 'inset 0px 0px 50px 10px rgba(0, 0, 0, 0.3)',
         'sunken': 'inset 0px 10px 3px -5px #000,' +
-            'inset -10px 0px 3px -5px #000,' +
-            'inset 0px -10px 3px -5px #000,' +
-            'inset 10px 0px 3px -5px #000',
+          'inset -10px 0px 3px -5px #000,' +
+          'inset 0px -10px 3px -5px #000,' +
+          'inset 10px 0px 3px -5px #000',
         'inner-lg': 'inset 0px 0px 20px 10px rgba(0, 0, 0, 0.3)',
       },
       colors: {
